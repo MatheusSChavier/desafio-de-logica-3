85c90128a4066db22b857a06bb8520b9fe10bd47
kenykenoei <matheuschavier112004@gmail.com>
2021-06-03 13:33:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+for(let X=1; X<=10; X++){
+    console.log(X)
+}
+let Y = 10
+while(Y>=0){
+    console.log(Y)
+     Y--
+}
+for(let Z=1; Z<=100; Z+=2){
+    console.log(Z)
+}
+for(let W=0; W<=100; W+=2){
+    console.log(W)
+}
